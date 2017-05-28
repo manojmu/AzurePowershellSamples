3164682d13573cdd71dd614413713dd6a4335735
manojmu <mamurali@microsoft.com>
2017-05-28 17:25:26 +0530

    Add files via upload

---
+++ CheckSRVSYSversion.ps1
@@ -0,0 +1,10 @@
+﻿$s = "%systemroot%\system32\drivers\srv.sys"
+$v = [System.Environment]::ExpandEnvironmentVariables($s)
+
+
+$versionInfo = (Get-Item $v).VersionInfo
+$versionInfo.FileVersion
+
+$versionString = "$($versionInfo.FileMajorPart).$($versionInfo.FileMinorPart).$($versionInfo.FileBuildPart).$($versionInfo.FilePrivatePart)"
+$stdout = New-Object System.Version($versionString) 
+return $stdout
